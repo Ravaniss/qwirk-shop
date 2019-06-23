0310dcbc9e8509fea97c53761a927914fd6e4220
Ravaniss <senechal.max06@gmail.com>
2019-06-23 13:30:24 +0200

    Delete vuejs logo and add cart & login icons

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,66 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'home' }">Home</router-link> |
-      <router-link :to="{ name: 'about' }">About</router-link> |
-      <router-link :to="{ name: 'user', params: { username: 'Max'}}">User</router-link>
-      <router-link :to="{ name: 'test'}">test</router-link>
+    <div class="outContent">
+      <div class="content">
+        <div id="nav">
+          <router-link :to="{ name: 'home' }" tag="div" class="logo"></router-link>
+          <router-link :to="{ name: 'auth' }" tag="div" id="loginImg"></router-link>
+          <router-link :to="{ name: 'cart' }" tag="div" id="cartImg"></router-link>
+          <div class="locale-changer">
+            <select v-model="$i18n.locale">
+              <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+  require('./assets/styles/style.scss')
+  import { Component, Vue } from 'vue-property-decorator'
+  export default class Home extends Vue {
+    langs = ['fr', 'en']
+  }
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  div#nav {
+    display: flex;
+    align-items: center;
+
+    div {
+      margin-right: 30px;
+    }
+
+    .settings {
+      display: flex;
+    }
+
+    div.logo {
+      height: 30px;
+      width: 30px;
+      border: solid grey 1px;
+    }
+
+    div#loginImg {
+      background-image: url('./assets/login.svg');
+      height: 25px;
+      width: 25px;
+      background-size: 25px;
+      background-repeat: no-repeat;
+    }
+
+    div#cartImg {
+      background-image: url('./assets/cart.svg');
+      height: 25px;
+      width: 30px;
+      background-size: 30px;
+      background-repeat: no-repeat;
     }
   }
-}
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,11 @@
 <template>
   <div class="home">
     <vue-topprogress ref="topProgress"></vue-topprogress>
-    <div class="locale-changer">
-      <select v-model="$i18n.locale">
-        <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
-      </select>
-    </div>
 
-    <h1>{{ $t("message") }}</h1>
-    Home page
+    <!--<h1>{{ $t("message") }}</h1>
     <BaseButton type="submit" buttonClass="fill-gradient">Submit</BaseButton>
-    <BaseButton @click="test()" buttonClass="fill-gray">Vendre</BaseButton>
+    <BaseButton @click="test()" buttonClass="fill-gray">Vendre</BaseButton>-->
 
-    <BaseMediaBox>
-      <h2 slot="heading">Heading</h2>
-      <p slot="paragraph">Paragraph</p>
-    </BaseMediaBox>
-
-    <BaseInput
-      label="Title"
-      v-model="title"
-      type="text"
-      placeholder="Your title here !"
-    ></BaseInput>
-
-    <BaseInput
-      label="Description"
-      v-model="desc"
-      type="text"
-      placeholder="Your description here !"
-    ></BaseInput>
-
-    <h1>{{ userName }}</h1>
-
-    <input type="number" v-model.number="incrementBy">
-    <button @click="incrementCount">+</button>
-    <p>{{ count }}</p>
   </div>
 </template>
 
@@ -46,26 +16,12 @@ import Vuex from 'vuex'
 import { vueTopprogress } from 'vue-top-progress'
 
 @Component({
-  methods: {
-    ...Vuex.mapActions([
-      'updateCount'
-    ])
-  },
-  computed: {
-    ...Vuex.mapGetters([
-      'userName',
-      'count'
-    ])
-  },
   components: {
     vueTopprogress
   }
 })
 export default class Home extends Vue {
   langs = ['fr', 'en']
-  incrementBy: number = 0
-  title: string = ''
-  desc: string = ''
 
   mounted () {
     this.$refs.topProgress.start()
@@ -80,10 +36,6 @@ export default class Home extends Vue {
         this.$refs.topProgress.fail()
         console.log(error)
       })
-  }
-
-  incrementCount () {
-    this.$store.dispatch('updateCount', this.incrementBy)
   }
 
   test() {
