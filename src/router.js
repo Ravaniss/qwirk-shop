30d3b99b20a2faaab15bc0084d546e86b983e700
Ravaniss <senechal.max06@gmail.com>
2019-06-23 13:30:52 +0200

    Start header shop

---
+++ src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import User from './views/User.vue';
+import NotFound from './views/NotFound.vue';
+Vue.use(Router);
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '*',
+            component: NotFound
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/test/:username',
+            name: 'user',
+            component: User,
+            props: true
+        }
+    ]
+});
