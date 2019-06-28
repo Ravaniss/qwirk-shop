3f27d967634b30dad6a43674faebce48b233bccd
Ravaniss <senechal.max06@gmail.com>
2019-06-28 22:07:07 +0200

    Shirt - Add the shirt view

---
+++ src/views/Shirt.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Hello world !
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import db from '../../db.json'
+
+  @Component
+  export default class Shirt extends Vue {
+    mounted () {
+      for (let i = 0; i < db.shirts.length; i++) {
+        if (i == this.$route.params.id) {
+          console.log(db.shirts[i].id, db.shirts[i].image, db.shirts[i].text)
+        }
+      }
+    }
+  }
+</script>
